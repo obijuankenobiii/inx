375377f7641b37e9c2e3212c7dc34d658bf5530b
Stratos Gerakakis <stratos@gerakakis.net>
2026-09-10 11:31:56 +0200

    Fixed typo: Ccnfirm -> Confirm

--- src/activity/page/SettingsActivity.cpp
+++ src/activity/page/SettingsActivity.cpp
@@ -73,7 +73,7 @@ std::vector<SettingInfo> buildSystemPageSettings(const bool x3) {
 
   settings.push_back(SettingInfo::Separator("Buttons", GroupType::DEVICE_BUTTONS));
   settings.push_back(SettingInfo::Enum("Front Button", &SystemSetting::frontButtonLayout,
-                                       {"Back, Ccnfirm, Left, Right", "Left, Right, Back, Confirm",
+                                       {"Back, Confirm, Left, Right", "Left, Right, Back, Confirm",
                                         "Left, Back, Confirm, Right", "Back, Confirm, Right, Left",
                                         "Left, Right, Confirm, Back"},
                                        GroupType::DEVICE_BUTTONS));
